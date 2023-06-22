849cb543929c5dc4b62cb943d3f41e1cdf838a1d
Vincent Lu <vlu@packsmartinc.com>
2023-06-22 18:02:11 -0400

    Daily Save

---
+++ ToolTip/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace ToolTip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bTipTest2201 = new DevExpress.XtraEditors.SimpleButton();
+            this.toolTipController1 = new DevExpress.Utils.ToolTipController(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(42, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // bTipTest2201
+            // 
+            this.bTipTest2201.Location = new System.Drawing.Point(42, 186);
+            this.bTipTest2201.Name = "bTipTest2201";
+            this.bTipTest2201.Size = new System.Drawing.Size(75, 23);
+            this.bTipTest2201.TabIndex = 1;
+            this.bTipTest2201.Text = "Dev 22.1";
+            this.bTipTest2201.Click += new System.EventHandler(this.bTipTest2201_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bTipTest2201);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraEditors.SimpleButton bTipTest2201;
+        private DevExpress.Utils.ToolTipController toolTipController1;
+    }
+}
+
---
+++ ToolTip/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToolTip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Winform tip
+            toolTip1.SetToolTip(panel1,"Test");
+
+            //Devexpress
+            toolTipController1.SetToolTip(bTipTest2201,"Test2");
+        }
+
+        private void bTipTest2201_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
